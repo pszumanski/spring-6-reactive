825cdb453e6b92f171cf61f44b7c2a3a1c64fa01
pszumanski <piotr.szumanski5@gmail.com>
2024-10-03 12:47:20 +0200

    Add not found status to patch operation, added test

--- src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
@@ -22,6 +22,15 @@ class BeerControllerTest {
     WebTestClient webTestClient;
 
     @Test
+    void testPatchNotFound() {
+        webTestClient.patch()
+                .uri(BeerController.BEER_PATH_ID, -1)
+                .body(Mono.just(BeerRepositoryTest.getTestBeer()), BeerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
     @Order(999)
     void testDeleteBeer() {
         webTestClient.delete()
